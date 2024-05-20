e80b07484f52bfff53e1316284cb31dbdb041e2b
sxzuzv <pofoxz@gmail.com>
2024-05-20 11:18:19 +0900

    Init: Web Socket

---
+++ socket/src/test/java/web/socket/SocketApplicationTests.java
@@ -0,0 +1,13 @@
+package web.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
